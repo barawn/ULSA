094fa5dc87657930b391c3ef1ab602b24e9cee11
Patrick Allison <barawn@osu-jtag2>
2026-02-12 09:51:58 -0500

    autodetect compiler and fix missing FFLAGS addition to so build

--- compileNE2001.sh
+++ compileNE2001.sh
@@ -2,5 +2,15 @@
 
 cd ULSA/NE2001/NE2001_4python/NE2001_4python/src.NE2001
 make clean
-make so
-
+if [ -x "$(command -v ifort)" ]; then
+    export FC=ifort
+    export FFLAGS=-O
+elif [ -x "$(command -v gfortran)" ]; then
+    export FC=gfortran
+    export FFLAGS="-O -std=legacy"
+elif [ -x "$(command -v f77)" ]; then
+    export FC=f77
+    export FFLAGS=-O
+fi
+echo FC=$FC FFLAGS=$FFLAGS make so
+FC=$FC FFLAGS="$FFLAGS" make so
